f034dac56304b1d5ff43f6e978d6d6fa185582d1
airjer22 <jclarkeokah@gmail.com>
2024-11-25 19:05:32 +1000

    Update page.tsx
    Trying to get logo to work

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className="w-64 h-64 relative mb-12">
           <Image
-            src="/northern-eagles-logo.png"
+            src="public/northern-eagles-logo.png"
             alt="Northern Eagles Logo"
             fill
             style={{ objectFit: 'contain' }}
